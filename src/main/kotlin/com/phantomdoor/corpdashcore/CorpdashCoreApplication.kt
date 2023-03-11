c4f0b1d6bfd957f0ca56eb234e4e59e301d61fba
Milos Zivkovic <developer.milos.zivkovic@gmail.com>
2023-03-11 11:30:22 +0100

    Initial commit

---
+++ src/main/kotlin/com/phantomdoor/corpdashcore/CorpdashCoreApplication.kt
@@ -0,0 +1,11 @@
+package com.phantomdoor.corpdashcore
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CorpdashCoreApplication
+
+fun main(args: Array<String>) {
+	runApplication<CorpdashCoreApplication>(*args)
+}
